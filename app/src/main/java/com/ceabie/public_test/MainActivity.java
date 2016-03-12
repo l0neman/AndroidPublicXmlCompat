f67b0526868bb6fdaf34079b49d96779f299d49d
ceabie <ceabie@gmail.com>
2016-03-12 16:17:40 +0800

    commit

---
+++ app/src/main/java/com/ceabie/public_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ceabie.public_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(publictest.ceabie.com.public_test.R.layout.activity_main);
+    }
+}
